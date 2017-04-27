08d9c0ec573edee215415795e81ec31381f31464
Michael Demerjian <michaeldemerjian@gmail.com>
2017-04-27 01:11:55 -0600

    turned the 8ball into jquery

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+$(document).ready( function() {
+  var $header = $('#header')
+  var $addEm = $('#addEm')
+  var answers = ['That is probablly a good idea', 'I think so', 'NO', 'Go for it',
+                  'Yes', 'You sure about that', 'hell yes', 'I would', 'Naaaa'];
+
+  $addEm.click( function() {
+    var randomId = answers[Math.floor(Math.random() * answers.length)];
+    var question = $('#question').val()
+    var li = '<li>' +'your question: ' + question + ',' + ' magic balls answer: '+ randomId + '</li>'
+    if( question === '')
+      alert('uh oh, type a question')
+
+    $('#test').text(randomId)
+    $('#responses').append(li)
+    $('#question').val('')
+  })
+})
+
+/*
 function addToList() {
   var questionBox = document.getElementById('question').value
   var list = document.getElementById('responses');
@@ -21,3 +41,4 @@ function add() {
     question.value = null;
   }
 };
+*/
